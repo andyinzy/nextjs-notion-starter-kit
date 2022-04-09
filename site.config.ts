8b57c62c307440ea01e3e0e6387c78ba99755e3f
andyzy <ziyuan_yin@foxmail.com>
2022-04-09 23:00:49 +0800

    andyzy blog: initial

--- site.config.ts
+++ site.config.ts
@@ -2,23 +2,23 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'ecb120de1a9b4f009d46c98c6372dca1',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Andy yin blog',
+  domain: 'www.andyzy.dev',
+  author: 'Andy',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
+  github: 'andyzy',
   linkedin: 'fisch2',
 
   // default notion icon and cover images for site-wide consistency (optional)
